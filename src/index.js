eb63bd33227912e25f8ea93d95085ab59cbc3552
vaibhavSaxena <119675076+CodeUncut@users.noreply.github.com>
2023-06-17 23:53:21 +0530

    Day1 complete this Project till saturday-monday.
    Let's Do Workout Together

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Using React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(<App />)
